31f4525224e3157db2c788f04269d19484947195
Oleg L <oleg.l@smatym.pro>
2019-02-18 06:17:06 +0600

    fix

--- Sources/BusinessLogicLayer/Mnemonic/MnemonicService.swift
+++ Sources/BusinessLogicLayer/Mnemonic/MnemonicService.swift
@@ -31,6 +31,7 @@ class MnemonicService: MnemonicServiceProtocol {
         case normal = 128
         case hight = 256
     }
+    
     func create(strenght: MnemonicService.Strenght, language: WordList) -> [String] {
         let byteCount = strenght.rawValue / 8
         var bytes = Data(count: byteCount)
@@ -40,6 +41,7 @@ class MnemonicService: MnemonicServiceProtocol {
         
         return create(entropy: bytes, language: language)
     }
+    
     func create(entropy: Data, language: WordList) -> [String] {
         print(entropy)
         let entropybits = String(entropy.flatMap { ("00000000" + String($0, radix: 2)).suffix(8) })
@@ -59,6 +61,7 @@ class MnemonicService: MnemonicServiceProtocol {
         }
         return mnemonic
     }
+    
     func createSeed(with mnemonic: [String], passphrase: String) throws -> Data {
         let words = WordList.english.words + WordList.russian.words
         guard !mnemonic.map({ words.contains($0) }).contains(false) else {
@@ -69,6 +72,7 @@ class MnemonicService: MnemonicServiceProtocol {
         let salt = ("mnemonic" + passphrase).toData()
         
         let hz = password.flatMap { UInt8(("00000000" + String($0, radix: 2)).suffix(8)) }
+        
         //let seed = PKCS5.PBKDF2(password: hz, salt: salt, iterations: 2048, keyLength: 64)//pbkdf2(password, salt: salt, iterations: 2048, keyLength: 64)
         return Data()
     }
